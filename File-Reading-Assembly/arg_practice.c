a5455e4c97e6064575aa1f6b0c96de029cafa523
Daymian Snowden <dsnowden25@gmail.com>
2025-05-21 19:36:13 -0400

    Added arguement code to arg_practice. Compiled to out file, tested, and ran.

--- File-Reading-Assembly/arg_practice.c
+++ File-Reading-Assembly/arg_practice.c
@@ -2,8 +2,13 @@
 #include <string.h>
 
 
-int main(int argc, char const *argv[])
-{
-    // Wite your code here to practice reading the program arguments. 
+int main(int argc, char const *argv[]) {
+    printf("Number of arguments is: %d\n", argc);
+    printf("Arguments: \n");
+    for(int i=0; i < argc; i++) {
+        printf("The argv[%d] = %s\n", i, argv[i]); 
+    printf("\n");
+    }
     return 0;
+    
 }
