f5d178a94046fcc7fef161732df2818d7a7c8980
Daymian Snowden <dsnowden25@gmail.com>
2025-05-21 19:58:56 -0400

    Generating assembly code

---
+++ File-Reading-Assembly/file_reading_practice.s
@@ -0,0 +1,75 @@
+	.file	"file_reading_practice.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"r"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	-32(%rbp), %rax
+	addq	$8, %rax
+	movq	(%rax), %rax
+	leaq	.LC0(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	fopen@PLT
+	movq	%rax, -16(%rbp)
+	movl	$1024, %edi
+	call	malloc@PLT
+	movq	%rax, -8(%rbp)
+	jmp	.L2
+.L3:
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+.L2:
+	movq	-16(%rbp), %rdx
+	movq	-8(%rbp), %rax
+	movl	$1024, %esi
+	movq	%rax, %rdi
+	call	fgets@PLT
+	testq	%rax, %rax
+	jne	.L3
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	free@PLT
+	movq	-16(%rbp), %rax
+	movq	%rax, %rdi
+	call	fclose@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
