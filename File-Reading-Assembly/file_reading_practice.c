bcdbd23c684d4c01086502451ce8488da56fa0e1
Daymian Snowden <dsnowden25@gmail.com>
2025-05-21 19:40:10 -0400

    Copied code from arg_practice for reference, along with some example code. (Added as comments)

--- File-Reading-Assembly/file_reading_practice.c
+++ File-Reading-Assembly/file_reading_practice.c
@@ -2,6 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+//for reference
+// int main(int argc, char const *argv[]) {
+//     printf("Number of arguments is: %d\n", argc);
+//     printf("Arguments: \n");
+//     for(int i=0; i < argc; i++) {
+//         printf("The argv[%d] = %s\n", i, argv[i]); 
+//     printf("\n");
+//     }
+//     return 0;
+    
+// }
+
+// ```c
+// FILE *input = fopen(argv[1],"r");
+// ```
+
+// ```c
+// char buff[BUFF_SIZE]; 
+
+// FILE *input = fopen(argv[1],"r");
+// char* line = malloc(BUFF_SIZE * sizeof(char));
+// while (fgets(line, BUFF_SIZE, input) != NULL) {
+//     // do the thing, line now has the line from input
+    
+// }
+
+// free(line); // don't forget to free memory!
+// fclose(input); // don't forget to close a file!
+// ```
+
 
 int main(int argc, char const *argv[])
 {
