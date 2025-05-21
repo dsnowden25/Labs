f2e62c6718599787fab5f4313de7ac14a8980adb
Daymian Snowden <dsnowden25@gmail.com>
2025-05-21 19:53:52 -0400

    Modifying code

--- File-Reading-Assembly/file_reading_practice.c
+++ File-Reading-Assembly/file_reading_practice.c
@@ -40,11 +40,11 @@ FILE *input = fopen(argv[1],"r");
 char* line = malloc(BUFF_SIZE * sizeof(char));
 while (fgets(line, BUFF_SIZE, input) != NULL) {
     // do the thing, line now has the line from input
-    
+    printf("%s",line);
 }
 
 free(line); // don't forget to free memory!
 fclose(input); // don't forget to close a file!
-    
-    return 0;
+
+return 0;
 }
